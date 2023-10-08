42365482474a6ccff5c7da961ad4140818c21f20
Rojal <rojalrojal220@gamail.com>
2023-10-08 23:41:53 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function loadNavigationAndFooter() {
+  // Load the navigation HTML
+  fetch('./navigation.html')
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector('nav').innerHTML = data;
+    })
+    .catch((error) => {
+      console.error('Error loading navigation:', error);
+    });
+
+  fetch('./footer.html')
+    .then((response) => response.text())
+    .then((data) => {
+      //   console.log(data);
+      document.querySelector('.footer').innerHTML = data;
+    })
+    .catch((error) => {
+      console.error('Error loading footer:', error);
+    });
+}
+
+// Call the function to load navigation and footer on page load
+document.addEventListener('DOMContentLoaded', loadNavigationAndFooter);
+
+// --------------------------------dark theme------------------
+var icon = document.getElementById('icon');
+icon.onclick = function () {
+  document.body.classList.toggle('dark');
+  if (document.body.classList.contains('dark')) {
+    icon.className = 'bi mi bi-brightness-high'; // Change to sun icon class when dark theme is active
+  } else {
+    icon.className = 'bi ni bi-moon-stars'; // Change back to moon icon class when dark theme is off
+  }
+};
